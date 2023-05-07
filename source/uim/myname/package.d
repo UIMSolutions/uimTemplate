9e1e79b9e18a74cb0f11ac712ab033de357a5b1e
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-07 15:02:01 +0200

    Updates

--- source/uim/myname/package.d
+++ source/uim/myname/package.d
@@ -1,7 +1,14 @@
 module uim.myname;
 
+public { // required uim libraries
+  import uim.core;
+  import uim.oop;
+}
+
 public { // myname packages
-    import uim.myname.classes;
-    import uim.myname.interfaces;
-    import uim.myname.mixins;
+  import uim.myname.classes;
+  import uim.myname.interfaces;
+  import uim.myname.helpers;
+  import uim.myname.mixins;
+  import uim.myname.tests;
 }
